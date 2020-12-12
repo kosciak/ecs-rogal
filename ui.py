1fad219c6d024c4f8d43f34e8c318c192bab8fc8
Wojciech Pietrzok <kosciak@kosciak.net>
2020-12-12 22:02:55 +0100

    ui.py - refactoring, fixing bugs, support for color palette

--- ui.py
+++ ui.py
@@ -1,5 +1,4 @@
 import collections
-from enum import Enum
 
 from geometry import Position, Size, WithSizeMixin, Rectangle
 from colors import RGB
@@ -12,14 +11,12 @@ This should make working on UI much easier, and maybe allow to use other cli/gra
 
 """
 
-DEFAULT_CH = ' '
+DEFAULT_CH = ord(' ')
 DEFAULT_FG = RGB(255, 255, 255).rgb
 DEFAULT_BG = RGB(0, 0, 0).rgb
 
-EMPTY_TILE = Tile.create(DEFAULT_CH, fg=DEFAULT_FG, bg=DEFAULT_BG)
 
-
-class Alignment(Enum):
+class Alignment:
     LEFT = 0    # tcod.LEFT
     RIGHT = 1   # tcod.RIGHT
     CENTER = 2  # tcod.CENTER
@@ -31,16 +28,22 @@ class AbstractTilesGrid(WithSizeMixin):
 
     __slots__ = ()
 
+    def empty_tile(self, colors):
+        fg = (colors and colors.fg) or DEFAULT_FG
+        bg = (colors and colors.bg) or DEFAULT_bG
+        return Tile.create(DEFAULT_CH, fg=fg, bg=bg)
+
     def clear(self, colors=None, *args, **kwargs):
         """Clear whole Rectangle wigh default values."""
-        if colors:
-            tile = Tile.create(DEFAULT_CH, fg=colors.fg, bg=colors.bg)
-        else:
-            tile = EMPTY_TILE
+        tile = self.empty_tile(colors)
         return self.fill(tile)
 
     def print(self, text, position, colors=None, alignment=None, *args, **kwargs):
-        """Print text on given position using colors."""
+        """Print text on given Position.
+        
+        Use Colors if provided, otherwise don't change alredy defined fg and bg.
+        
+        """
         raise NotImplementedError()
 
     def draw(self, tile, position, size=None, *args, **kwargs):
@@ -67,6 +70,8 @@ class AbstractTilesGrid(WithSizeMixin):
         """Draw image on given position."""
         raise NotImplementedError()
 
+    # TODO: blit_from, blit_to
+
 
 class Panel(AbstractTilesGrid, Rectangle):
 
@@ -88,6 +93,10 @@ class Panel(AbstractTilesGrid, Rectangle):
         return Position(self.x+position.x, self.y+position.y)
 
     # NOTE: Only position is translated, it is NOT checked if prints are outside of Panel!
+
+    def create_panel(self, position, size):
+        offset = self._root_offset(position)
+        return self.root.create_panel(offset, size)
 
     def print(self, text, position, colors=None, alignment=None, *args, **kwargs):
         """Print text on given position using colors."""
@@ -118,22 +127,10 @@ class Panel(AbstractTilesGrid, Rectangle):
         offset = self._root_offset(position)
         return self.root.image(image, offset, *args, **kwargs)
 
-    # TODO: Needs rework!
-    def blit_from(self, x, y, src, *args, **kwargs):
-        # RULE: Use keyword arguments for dest_x, dest_y, width height
-        # NOTE: src MUST be tcod.Console!
-        parent_x, parent_y = self._translate_parent(x, y)
-        return self.root.blit_from(parent_x, parent_y, src, *args, **kwargs)
-
-    def blit_to(self, x, y, dest, *args, **kwargs):
-        # RULE: Use keyword arguments for dest_x, dest_y, width height
-        # NOTE: dest MUST be tcod.Console!
-        parent_x, parent_y = self._translate_parent(x, y)
-        return self.root.blit_to(parent_x, parent_y, dest, *args, **kwargs)
-
     # Other methods
 
     # TODO: get_tile(self, position)
+    # TODO: get_char(self, position)
     # TODO: get_colors(self, position)
 
     def __repr__(self):
@@ -154,27 +151,23 @@ class HorizontalContainer(Container):
         if height and height < 1:
             height = int(self.height * height)
         if top:
-            top = SplittablePanel(
-                self.root, 
-                self._root_offset(Position.ZERO), 
+            top = self.create_panel(
+                Position.ZERO, 
                 Size(self.width, self.height-height),
             )
-            bottom = SplittablePanel(
-                self.root, 
-                self._root_offset(Position(0, self.height-height)), 
+            bottom = self.create_panel(
+                Position(0, self.height-height), 
                 Size(self.width, height),
             )
             self.panels = [top, bottom]
             return top, bottom
         elif bottom:
-            top = SplittablePanel(
-                self.root, 
-                self._root_offset(Position.ZERO),
+            top = self.create_panel(
+                Position.ZERO,
                 Size(self.width, height),
             )
-            bottom = SplittablePanel(
-                self.root, 
-                self._root_offset(Position(0, height)),
+            bottom = self.create_panel(
+                Position(0, height),
                 Size(self.width, self.height-height),
             )
             self.panels = [top, bottom]
@@ -188,27 +181,23 @@ class VerticalContainer(Container):
         if width and width < 1:
             width = int(self.width * width)
         if left:
-            left = SplittablePanel(
-                self.root, 
-                self._root_offset(Position.ZERO),
+            left = self.create_panel(
+                Position.ZERO,
                 Size(self.width-width, self.height),
             )
-            right = SplittablePanel(
-                self.root, 
-                self._root_offset(Position(self.width-width, 0)), 
+            right = self.create_panel(
+                Position(self.width-width, 0), 
                 Size(width, self.height),
             )
             self.panels = [left, right]
             return left, right
         elif right:
-            left = SplittablePanel(
-                self.root, 
-                self._root_offset(Position.ZERO), 
+            left = self.create_panel(
+                Position.ZERO, 
                 Size(width, self.height),
             )
-            right = SplittablePanel(
-                self.root, 
-                self._root_offset(Position(width, 0)), 
+            right = self.create_panel(
+                Position(width, 0), 
                 Size(self.width-width, self.height),
             )
             self.panels = [left, right]
@@ -244,100 +233,82 @@ class SplittablePanel(Panel):
             return self.split_horizontal(top=top, bottom=bottom)
 
 
-FrameDecorations = collections.namedtuple(
-    'FrameDecorations', [
-        'top', 'bottom', 'side',
-        'top_left', 'top_right',
-        'bottom_left', 'bottom_right',
-    ])
+class RootPanel(SplittablePanel):
 
+    def __init__(self, console, palette):
+        super().__init__(self, Position.ZERO, Size(console.width, console.height))
+        self.console = console
+        self.palette = palette
+        self.clear()
 
-DECORATIONS_ASCII = FrameDecorations(*"=-|..''")
+    def empty_tile(self, colors):
+        fg = self.rgb(colors and colors.fg) or self.palette.fg
+        bg = self.rgb(colors and colors.bg) or self.palette.bg
+        return Tile.create(DEFAULT_CH, fg=fg, bg=bg)
 
+    def create_panel(self, position, size):
+        return SplittablePanel(self, position, size)
 
-class Frame(Container):
-    # TODO: Custom window decorations?
-    # TODO: Setting title, adding buttons(?) on top/bottom border, scrollbars?
-    #       For example setting things like: 
-    #       .- Title ------[X]-.
-    #       |                  |
-    #       '-----------(more)-'
-
-    def draw_decorations(self, decorations=None, *args, **kwargs):
-        if not decorations:
-            self.draw_frame(0, 0, width=self.width, height=self.height, *args, **kwargs)
-        else:
-            self.draw_rect(1, 0, width=self.width-2, height=1, ch=ord(decorations.top))
-            self.draw_rect(1, self.height-1, width=self.width-2, height=1, ch=ord(decorations.bottom))
-            self.draw_rect(0, 1, width=1, height=self.height-2, ch=ord(decorations.side))
-            self.draw_rect(self.width-1, 1, width=1, height=self.height-2, ch=ord(decorations.side))
-            self.put_char(0, 0, ord(decorations.top_left))
-            self.put_char(self.width-1, 0, ord(decorations.top_right))
-            self.put_char(0, self.height-1, ord(decorations.bottom_left))
-            self.put_char(self.width-1, self.height-1, ord(decorations.bottom_right))
-
-
-class Window:
-    # TODO: For now it's just proof of concept
-    # NOTE: splitting window.panel leaves original panel reference!
-
-    def __init__(self, parent, x, y, width, height, decorations):
-        self.frame = Frame(parent, Position(x, y), Size(width, height))
-        self.frame.draw_decorations(decorations)
-        self.panel = SplittablePanel(self.frame, Position(1, 1), Size(width-2, height-2))
-        self.frame.panels.append(self.panel)
+    def rgb(self, color):
+        if not color:
+            return None
+        if isinstance(color, tuple):
+            if len(color) == 3:
+                return color
+            else:
+                return color[:3]
+        return self.palette.get(color).rgb
 
 
 # TODO: Move to ui.tcod_wrapper ?
-class RootPanel(SplittablePanel):
-
-    def __init__(self, console):
-        super().__init__(self, Position.ZERO, Size(console.width, console.height))
-        self.console = console
+class TcodRootPanel(RootPanel):
 
     def __str__(self):
         return str(self.console)
 
-    def _translate_root(self, x, y):
-        return x, y
-
     def clear(self, colors=None, *args, **kwargs):
-        fg = colors and colors.fg or DEFAULT_FG
-        bg = colors and colors.bg or DEFAULT_BG
+        fg = self.rgb(colors and colors.fg) or self.palette.fg.rgb
+        bg = self.rgb(colors and colors.bg) or self.palette.bg.rgb
         return self._clear(fg=fg, bg=bg, *args, **kwargs)
 
     def print(self, text, position, colors=None, alignment=None, *args, **kwargs):
-        fg = colors and colors.fg or DEFAULT_FG
-        bg = colors and colors.bg
-        alignment = alignment or Alignment.LEFT.value
+        # NOTE: Do NOT change already set colors!
+        fg = self.rgb(colors and colors.fg)
+        bg = self.rgb(colors and colors.bg)
+        alignment = alignment or Alignment.LEFT
         return self._print(
             position.x, position.y, text, fg=fg, bg=bg, alignment=alignment, *args, **kwargs)
 
     def draw(self, tile, position, size=None, *args, **kwargs):
+        fg = self.rgb(tile.fg)
+        bg = self.rgb(tile.bg)
         if size:
             return self._draw_rect(
                 position.x, position.y, size.width, size.height, tile.code_point, 
-                fg=tile.fg, bg=tile.bg, *args, **kwargs)
+                fg=fg, bg=bg, *args, **kwargs)
         else:
             return self._print(
-                position.x, position.y, tile.char, fg=tile.fg, bg=tile.bg, *args, **kwargs)
+                position.x, position.y, tile.ch, fg=fg, bg=bg, *args, **kwargs)
 
     def paint(self, colors, position, size=None, *args, **kwargs):
+        fg = self.rgb(colors.fg)
+        bg = self.rgb(colors.bg)
         if size:
-            if colors.fg:
-                self.console.fg[position.x:position.x+size.width, position.y:position.y+size.height] = colors.fg
-            if colors.bg:
-                self.console.bg[position.x:position.x+size.width, position.y:position.y+size.height] = colors.bg
+            if fg:
+                self.console.fg[position.x:position.x+size.width, position.y:position.y+size.height] = fg
+            if bg:
+                self.console.bg[position.x:position.x+size.width, position.y:position.y+size.height] = bg
         else:
-            if colors.fg:
-                self.console.fg[position] = colors.fg
-            if colors.bg:
-                self.console.bg[position] = colors.bg
+            if fg:
+                self.console.fg[position] = fg
+            if bg:
+                self.console.bg[position] = bg
 
     def image(self, image, position, *args, **kwargs):
         return self._draw_semigraphics(
             image, position.x, position.y, *args, **kwargs)
 
+    # TODO: Needs rework using Position, Size!
     def blit_from(self, x, y, src, *args, **kwargs):
         # RULE: Use keyword arguments for dest_x, dest_y, width height
         # NOTE: src MUST be tcod.Console!
@@ -347,9 +318,6 @@ class RootPanel(SplittablePanel):
         # RULE: Use keyword arguments for dest_x, dest_y, width height
         # NOTE: dest MUST be tcod.Console!
         self.console.blit(dest=dest, src_x=x, src_y=y, *args, **kwargs)
-
-    def create_window(self, x, y, width, height): #, decorations=None):
-        return SplittablePanel(self, x, y, width, height) #, decorations)
 
     # NOTE: tcod.Console print/draw related methods:
 
@@ -434,7 +402,7 @@ class RootPanel(SplittablePanel):
             fg: Tuple[int, int, int] = Ellipsis, 
             bg: Tuple[int, int, int] = Ellipsis)
         """
-        ch = ord(ch or DEFAULT_CH)
+        ch = ch or DEFAULT_CH
         return self.console.clear(ch, fg=fg, bg=bg, *args, **kwargs)
 
     # blit(
@@ -475,13 +443,10 @@ class RootPanel(SplittablePanel):
             print(''.join(row_txt))
 
 
-# TODO: alignment, bg_blend
+# TODO: Support for bg_blend, learn how it works in tcod
 
 # TODO: ansi-like color control codes - "%c%c%c%cFoo%c" % (tcod.COLCTRL_FORE_RGB, *tcod.white, tcod.COLCTRL_STOP)
 
-# TODO: Rework Window class - now it's a mess
-
-# TODO: frame() should produce FramedPanel with Frame and Panel inside
 # TODO: frames overlapping, and fixing overlapped/overdrawn characters to merge borders/decorations
 
 # TODO: Keep track of Z-order of Windows?
@@ -489,3 +454,51 @@ class RootPanel(SplittablePanel):
 # TODO: Scrollable Panels?
 # TODO: Window/Dialog?
 # TODO: event handlers?
+
+
+# TODO: frame() should produce FramedPanel with Frame and Panel inside
+# TODO: Rework FramedWindow class - now it's a mess
+
+FrameDecorations = collections.namedtuple(
+    'FrameDecorations', [
+        'top', 'bottom', 'side',
+        'top_left', 'top_right',
+        'bottom_left', 'bottom_right',
+    ])
+
+
+DECORATIONS_ASCII = FrameDecorations(*"=-|..''")
+
+
+class Frame(Container):
+    # TODO: Custom window decorations?
+    # TODO: Setting title, adding buttons(?) on top/bottom border, scrollbars?
+    #       For example setting things like: 
+    #       .- Title ------[X]-.
+    #       |                  |
+    #       '-----------(more)-'
+
+    def draw_decorations(self, decorations=None, *args, **kwargs):
+        if not decorations:
+            self.draw_frame(0, 0, width=self.width, height=self.height, *args, **kwargs)
+        else:
+            self.draw_rect(1, 0, width=self.width-2, height=1, ch=ord(decorations.top))
+            self.draw_rect(1, self.height-1, width=self.width-2, height=1, ch=ord(decorations.bottom))
+            self.draw_rect(0, 1, width=1, height=self.height-2, ch=ord(decorations.side))
+            self.draw_rect(self.width-1, 1, width=1, height=self.height-2, ch=ord(decorations.side))
+            self.put_char(0, 0, ord(decorations.top_left))
+            self.put_char(self.width-1, 0, ord(decorations.top_right))
+            self.put_char(0, self.height-1, ord(decorations.bottom_left))
+            self.put_char(self.width-1, self.height-1, ord(decorations.bottom_right))
+
+
+class FramedWindow:
+    # TODO: For now it's just proof of concept
+    # NOTE: splitting window.panel leaves original panel reference!
+
+    def __init__(self, parent, x, y, width, height, decorations):
+        self.frame = Frame(parent, Position(x, y), Size(width, height))
+        self.frame.draw_decorations(decorations)
+        self.panel = SplittablePanel(self.frame, Position(1, 1), Size(width-2, height-2))
+        self.frame.panels.append(self.panel)
+
