039fbbd70683ccfb629622f437bfa425bd253485
Wojciech Pietrzok <kosciak@kosciak.net>
2020-12-18 16:52:46 +0100

    main.py - render_camera() seems to be mostly done, rewritten and simplified

--- main.py
+++ main.py
@@ -27,8 +27,10 @@ log = logging.getLogger('rogal.main')
 
 CONSOLE_SIZE = Size(80, 50)
 
-CAMERA_SIZE = Size(10, 10)
-LEVEL_SIZE = Size(12,8)
+SCROLLABLE_CAMERA = True
+
+CAMERA_SIZE = Size(12, 12)
+LEVEL_SIZE = Size(14,9)
 
 
 def render_message_log(panel):
@@ -36,70 +38,72 @@ def render_message_log(panel):
     for offset, msg in enumerate(reversed(logs.LOGS_HISTORY), start=1):
         if offset > panel.height:
             break
-        panel.print(msg.message, Position(1, panel.height-offset), colors=Colors(fg=msg.fg))
+        panel.print(msg.message, Position(0, panel.height-offset), colors=Colors(fg=msg.fg))
 
 
-def render_camera(panel, ecs, level, player):
+def render_camera(panel, ecs, level, player, scrollable=SCROLLABLE_CAMERA):
+    """Render level contets viewed through (scrollable) camera."""
     import numpy as np
-    from terrain import Terrain
     from geometry import Position, Rectangle
     import tiles
 
-    player_position = ecs.manage(components.Location).get(player).position
+    # Select what camera should be centered on
+    if scrollable:
+        # Player position, relative to level
+        cam_center = player.get(components.Location).position
+    else:
+        # Level center
+        cam_center = level.center
 
-    tiles = {
-        Terrain.STONE_WALL.id:     tiles.STONE_WALL,
-        Terrain.STONE_FLOOR.id:    tiles.STONE_FLOOR,
-    }
-
-    log.debug('--- Rendering level ---')
-    log.debug(f'Panel           : {panel}')
-    log.debug(f'Level           : {level.size}')
-    centers_offset = panel.center - level.center
-    render_offset = Position(
-        min((panel.width-level.width)/2, centers_offset.x),
-        min((panel.height-level.height)/2, centers_offset.y),
+    # Camera, position relative to level
+    camera = Rectangle(
+        Position(cam_center.x-panel.width//2, cam_center.y-panel.height//2),
+        panel.size
     )
-    log.debug(f'Render offset   : {render_offset}')
 
-    level_rectangle = Rectangle(panel.position+render_offset, level.size)
-    log.debug(f'Level rect      : {level_rectangle}')
-    #intersection = panel & level_rectangle
-    intersection = level_rectangle & panel
-    log.debug(f'Intersection    : {intersection}')
+    # Part of level that is covered by camera
+    cam_coverage = camera & Rectangle(Position.ZERO, level.size)
+    if not cam_coverage:
+        # Nothing to display, nothing to do here...
+        return
 
-    # Render terrain
-    slice_x = slice(abs(min(0, render_offset.x)), min(level.width, level.width+render_offset.x))
-    slice_y = slice(abs(min(0, render_offset.y)), min(level.height, level.height+render_offset.y))
-    print('???', slice_x, slice_y)
-    terrain = level.terrain[slice_x, slice_y]
-    print('???', terrain.shape)
-    mask_offset = Position(max(0, render_offset.x), max(0, render_offset.y))
-    for tile_id in np.unique(terrain):
-        mask = terrain == tile_id
-        tile = tiles.get(tile_id)
+    # TODO: Handle level.visible, level.revealed flags
+
+    # Slice level.terrain to get part that is covered by camera
+    terrain = level.terrain[cam_coverage.x:cam_coverage.x2, cam_coverage.y:cam_coverage.y2]
+
+    # Offset for drawing a terrain tile with a mask
+    # It's mirrored camera.position but only with x,y values > 0
+    mask_offset = Position(max(0, camera.x*-1), max(0, camera.y*-1))
+
+    # Draw TERRAIN tiles
+    for terrain_id in np.unique(terrain):
+        mask = terrain == terrain_id
+        tile = tiles.TERRAIN.get(terrain_id)
         panel.mask(tile, mask, mask_offset)
 
-    # Render all renderable entities
+    # Draw all renderable ENTITIES, in order described by Renderable.render_order
     locations = ecs.manage(components.Location)
     renderables = ecs.manage(components.Renderable)
     for renderable, location in sorted(ecs.join(renderables, locations)):
-        render_position = render_offset + location.position
-        if not panel.position+render_position in panel:
+        if not location.map_id == level.id:
+            # Not on the map/level we are rendering, skip!
             continue
+        if not location.position in cam_coverage:
+            # Not inside area covered by camera, skip!
+            continue
+        render_position = location.position.offset(camera.position)
         panel.draw(renderable.tile, render_position)
 
 
 def render(wrapper, root_panel, ecs, level, player):
     root_panel.clear()
-    camera, message_log = root_panel.split(bottom=5)
-    camera_position = Position(camera.center.x-CAMERA_SIZE.width/2, camera.center.y-CAMERA_SIZE.height/2)
-    camera = root_panel.create_panel(camera_position, CAMERA_SIZE)
+    camera, message_log = root_panel.split(bottom=7)
+    #camera = root_panel.create_panel(Position(10,10), CAMERA_SIZE)
 
-    render_message_log(message_log)
+    render_message_log(message_log.framed('logs'))
 
-    render_camera(camera, ecs, level, player)
-    root_panel._draw_frame(camera.x-1, camera.y-1, camera.width+2, camera.height+2, 'mapcam', clear=False)
+    render_camera(camera.framed('mapcam'), ecs, level, player)
 
     # Show rendered panel
     wrapper.flush(root_panel)
@@ -131,6 +135,7 @@ def handle_events(wrapper, ecs, level, player):
 def loop(wrapper, root_panel, ecs, level, player):
     while True:
         # Render
+        # TODO: No need to render after EVERY single event!
         render(wrapper, root_panel, ecs, level, player)
 
         # Handle user input
@@ -160,6 +165,10 @@ def main():
         level = game_map.generate(LEVEL_SIZE)
         player = entities.create_player(ecs)
         entities.spawn(ecs, player, level, level.center)
+        for offset in [(-2,-2), (2,2), (-3,3), (3,-3)]:
+            monster = entities.create_monster(ecs)
+            position = level.center + Position(*offset)
+            entities.spawn(ecs, monster, level, position)
 
         loop(wrapper, root_panel, ecs, level, player)
 
