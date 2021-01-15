f378a2743a4d3534bb6cfaa71bddf8c48462659e
Wojciech Pietrzok <kosciak@kosciak.net>
2021-01-15 21:28:32 +0100

    geometry.py - chebyshev_distance and manhattan_distance added

--- rogal/geometry.py
+++ rogal/geometry.py
@@ -10,6 +10,24 @@ def euclidean_distance(position, other):
     return math.hypot(delta_x, delta_y)
 
 
+def chebyshev_distance(position, other):
+    """Return Chebyshev distance between points.
+
+    AKA chessboard distance, both ordinal and diagonal movements has the same cost.
+
+    """
+    return max(abs(position.x-other.x), abs(position.y-other.y))
+
+
+def manhattan_distance(position, other):
+    """Return Manhattan distance between points.
+
+    AKA taxicab distance - sum of absolute differences. Diagonal movement has twice the cost.
+
+    """
+    return abs(position.x-other.x) + abs(position.y-other.y)
+
+
 class Direction(Enum):
 
     """Direction on 2D plane."""
