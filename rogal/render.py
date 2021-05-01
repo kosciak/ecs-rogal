f840538588f7a3f8c4afeebd3a39aaef7a00c3f4
Wojciech Pietrzok <kosciak@kosciak.net>
2021-05-01 15:16:36 +0200

    render.py - refactoring Camera to be console.Widget friendly

--- rogal/render.py
+++ rogal/render.py
@@ -7,7 +7,7 @@ from . import logs
 
 from . import bitmask
 from . import components
-from .geometry import Rectangular, Position, Size, Rectangle
+from .geometry import Position, WithPositionMixin, Size, Rectangle
 from .tiles import RenderOrder, Colors, Tile
 from . import terrain
 
@@ -31,7 +31,7 @@ SCROLLABLE_CAMERA = False
 SHOW_BOUNDARIES = True
 
 
-class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
+class Camera(WithPositionMixin, toolkit.Renderer, toolkit.UIElement):
 
     def __init__(self, ecs,
                  scrollable=SCROLLABLE_CAMERA, show_boundaries=SHOW_BOUNDARIES):
@@ -41,9 +41,7 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
 
         self.tileset = self.ecs.resources.tileset
 
-        self.panel = None
-
-        self.position = Position.ZERO
+        self.cam_area = None
 
         self.scrollable = scrollable
         self.show_boundaries = show_boundaries
@@ -52,17 +50,17 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
         self.bitmasked_walls = self.tileset.bitmasks.WALLS_DLINE
 
     @property
-    def size(self):
-        return self.panel.size
+    def position(self):
+        return self.cam_area.position
 
-    def calc_position(self, level, position=None):
+    def camera_position(self, panel, level, position=None):
         """Select what camera should be centered on."""
         if not position or not self.scrollable:
             # Use center of given level
             position = level.center
-        self.position = Position(
-            position.x-self.panel.width//2,
-            position.y-self.panel.height//2
+        camera_position = Position(
+            position.x-panel.width//2,
+            position.y-panel.height//2
         )
 
         # For level-centered camera need to move camera if level.size > camera.size and approaching edge
@@ -70,19 +68,20 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
         # TODO: Maybe use viewshed.view_range for adjustments? Whole viewshed should be rendered
         # Adjust top-left corner
         adjusted_position = Position(
-            min(self.x, position.x-1),
-            min(self.y, position.y-1)
+            min(camera_position.x, position.x-1),
+            min(camera_position.y, position.y-1)
         )
         # Adjust bottom-right corner
         adjust_move = Position(
-        max(0, 2+position.x-self.x2),
-        max(0, 2+position.y-self.y2),
+            max(0, 2+position.x-(camera_position.x+panel.width)),
+            max(0, 2+position.y-(camera_position.y+panel.height)),
         )
-        self.position = adjusted_position + adjust_move
 
-    def get_coverage(self, level):
+        return adjusted_position + adjust_move
+
+    def get_coverage(self, panel, level):
         """Return part of the level that is covered by camera."""
-        coverage = self & level
+        coverage = self.cam_area & level
         return coverage
 
     def get_covered(self, array, coverage):
@@ -98,7 +97,7 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
         # NOTE: We don't want bitmasking to spoil not revealed terrain!
         return bitmask.bitmask_walls(walls_mask, revealed)
 
-    def draw_boundaries(self, level_size):
+    def draw_boundaries(self, panel, level_size):
         """Draw BOUNDARIES of the level."""
         if not self.show_boundaries:
             return
@@ -109,14 +108,16 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
             Rectangle(Position(-1, 0), Size(1, level_size.height)),
             Rectangle(Position(level_size.width, 0), Size(1, level_size.height)),
         ]:
-            intersection = self & boundary
+            intersection = self.cam_area & boundary
             if intersection:
                 boundaries.update(intersection.positions)
         for position in boundaries:
             tile = self.tileset.get('BOUNDARY').visible
-            self.panel.draw(tile, position.offset(self.position))
+            render_position = position.offset(self.position)
+            panel.draw(tile, render_position)
 
     def draw_terrain_tile(self,
+        panel,
         terrain_mask, visible, revealed, mask_offset,
         renderable, ch=None,
     ):
@@ -126,7 +127,7 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
             tile = renderable.tile_visible
             if ch is not None:
                 tile = Tile.create(ch, tile.fg, tile.bg)
-            self.panel.mask(tile, mask, mask_offset)
+            panel.mask(tile, mask, mask_offset)
 
         # Revealed but not visible
         mask = terrain_mask & revealed
@@ -134,9 +135,10 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
             tile = renderable.tile_revealed
             if ch is not None:
                 tile = Tile.create(ch, tile.fg, tile.bg)
-            self.panel.mask(tile, mask, mask_offset)
+            panel.mask(tile, mask, mask_offset)
 
     def draw_bitmasked_terrain_tile(self,
+        panel,
         walls_mask, terrain_mask, visible, revealed, mask_offset,
         renderable,
     ):
@@ -144,11 +146,12 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
             ch = self.bitmasked_walls[bitmask_value]
             mask = terrain_mask & (walls_mask == bitmask_value)
             self.draw_terrain_tile(
+                panel,
                 mask, visible, revealed, mask_offset,
                 renderable, ch,
             )
 
-    def draw_terrain(self, level_id, terrain, coverage, revealed, visible):
+    def draw_terrain(self, panel, level_id, terrain, coverage, revealed, visible):
         """Draw TERRAIN tiles."""
 
         # Bitshift masking for terrain.Type.WALL terrain
@@ -156,7 +159,10 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
 
         # Offset for drawing a terrain tile with a mask
         # It's mirrored camera.position but only with x,y values > 0
-        mask_offset = Position(max(0, self.x*-1), max(0, self.y*-1))
+        mask_offset = Position(
+            max(0, self.x*-1),
+            max(0, self.y*-1)
+        )
 
         revealed_not_visible = revealed ^ visible
 
@@ -172,17 +178,19 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
 
             if terrain_id >> 4 == self.walls_terrain_type:
                 self.draw_bitmasked_terrain_tile(
+                    panel,
                     walls_mask,
                     terrain_mask, visible, revealed_not_visible, mask_offset,
                     renderable,
                 )
             else:
                 self.draw_terrain_tile(
+                    panel,
                     terrain_mask, visible, revealed_not_visible, mask_offset,
                     renderable,
                 )
 
-    def draw_entities(self, level_id, coverage, revealed, visible):
+    def draw_entities(self, panel, level_id, coverage, revealed, visible):
         """Draw all renderable ENTITIES, in order described by Renderable.render_order."""
         renderables = self.ecs.manage(components.Renderable)
         locations = self.ecs.manage(components.Location)
@@ -209,13 +217,11 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
             if tile is not None:
                 render_position = location.position.offset(self.position)
                 if not tile.ch:
-                    self.panel.paint(tile.colors, render_position)
+                    panel.paint(tile.colors, render_position)
                 else:
-                    self.panel.draw(tile, render_position)
+                    panel.draw(tile, render_position)
 
     def render(self, panel, actor=None, location=None):
-        self.panel = panel
-
         position = None
         fov = None
         seen = None
@@ -239,9 +245,10 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
         else:
             seen = self.spatial.revealable(location.level_id)
 
-        self.calc_position(level, position)
+        position = self.camera_position(panel, level, position)
+        self.cam_area = Rectangle(position, panel.size)
 
-        coverage = self.get_coverage(level)
+        coverage = self.get_coverage(panel, level)
         if not coverage:
             # Nothing to display, nothing to do here...
             return
@@ -253,11 +260,11 @@ class Camera(Rectangular, toolkit.Renderer, toolkit.UIElement):
         terrain = self.get_covered(level.terrain, coverage)
 
         with perf.Perf(self.draw_boundaries):
-            self.draw_boundaries(level.size)
+            self.draw_boundaries(panel, level.size)
         with perf.Perf(self.draw_terrain):
-            self.draw_terrain(location.level_id, terrain, coverage, revealed, visible)
+            self.draw_terrain(panel, location.level_id, terrain, coverage, revealed, visible)
         with perf.Perf(self.draw_entities):
-            self.draw_entities(location.level_id, coverage, revealed, visible)
+            self.draw_entities(panel, location.level_id, coverage, revealed, visible)
 
 
 class MessageLog(toolkit.Renderer, toolkit.UIElement):
